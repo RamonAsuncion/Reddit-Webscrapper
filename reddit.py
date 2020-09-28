015afcef912ecd5ba2e45f291ea0f3e84b098ba3
Ramon Asuncion <asuncionbatista@gmail.com>
2020-09-27 22:37:01 -0400

    Commit

--- reddit.py
+++ reddit.py
@@ -1,2 +1,17 @@
 from bs4 import BeautifulSoup
 import requests
+# test link https://www.reddit.com/r/ProgrammerHumor/comments/j0uoo1/the_struggle_gets_real_when_the_family_gets/
+
+url = input('Please input the url: ')
+source = requests.get(url)
+soup = BeautifulSoup(source.content, 'lxml')
+
+print(url)
+
+
+def main():
+    return
+
+
+if __name__ == 'main':
+    main()
