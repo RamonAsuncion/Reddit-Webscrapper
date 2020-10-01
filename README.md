eb3492a141269fc9564c4d5679c228920bde818b
Ramon Asuncion <asuncionbatista@gmail.com>
2020-09-30 23:46:12 -0400

    Update to README.md + added gitignore

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 ## Reddit Web scraper 
 Accept user input:
 - User types in a link to a post and the program finds the top 3 comments from hot/best/new etc...)
+
+- has to ignore the sub comments 
+
+- seperate each get element - one function for best... etc 
+
+- needs to get users, points, how long ago, and comment 
--- reddit.py
+++ reddit.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 import requests
 # test link https://www.reddit.com/r/ProgrammerHumor/comments/j0uoo1/the_struggle_gets_real_when_the_family_gets/
 
+# Sort by: Best, Top, New, Controversial, Old, Q&A and grabs the first top element (first top comment)
+
 url = input('Please input the url: ')
 source = requests.get(url)
 soup = BeautifulSoup(source.content, 'lxml')
@@ -10,6 +12,7 @@ print(url)
 
 
 def main():
+    data_collected_from = []
     return
 
 
