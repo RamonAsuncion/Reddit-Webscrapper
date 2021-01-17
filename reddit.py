7265077df0a4dba4e639af5a63380c92833a5190
Ramon Asuncion <asuncionbatista@gmail.com>
2021-01-16 23:40:11 -0500

    Ask for link, correction to if __name__

--- reddit.py
+++ reddit.py
@@ -1,20 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
+from time import sleep
+
+
 # test link https://www.reddit.com/r/ProgrammerHumor/comments/j0uoo1/the_struggle_gets_real_when_the_family_gets/
 
 # Sort by: Best, Top, New, Controversial, Old, Q&A and grabs the first top element (first top comment)
 
-url = input('Please input the url: ')
-source = requests.get(url)
-soup = BeautifulSoup(source.content, 'lxml')
 
-print(url)
+def ask_for_link():
+    url = input('Please input the url: ')
+    print(url)
+    source = requests.get(url)
+    soup = BeautifulSoup(source.content, 'lxml')
 
 
 def main():
-    data_collected_from = []
-    return
+    ask_for_link()
 
 
-if __name__ == 'main':
+if __name__ == '__main__':
     main()
