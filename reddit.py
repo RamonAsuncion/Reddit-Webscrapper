7b14f7ebdfddb5174ee3e74229cf2504aa072e52
Ramon Asuncion <asuncionbatista@gmail.com>
2021-01-25 23:46:32 -0500

    Autocompletes https:// for url and modification of checking status:

--- reddit.py
+++ reddit.py
@@ -2,21 +2,27 @@ from bs4 import BeautifulSoup
 import requests
 from time import sleep
 import urllib.request
+from urllib.parse import urlparse
 
 
 # Need Regex to check if valid link and request 200 to check if link is working
 def ask_for_link():
     url = input('Please input the url: ')
+
+    if not urlparse(url).scheme:
+        url = 'https://' + url
+
     print("Debug: {}".format(url))
 
-    check_response = requests.get(url)
+    # Request
+    check_response = urllib.request.urlopen(url).getcode()
+
+    if check_response == 200:
+        print('Debug: Reached!')
+    else:
+        print('Debug: Failed')
 
     # For testing check for page request will be probably implemented
-    if check_response.status_code == 200:
-        print("Reached!")
-    else:
-        print("Failed")
-
     source = requests.get(url)
     soup = BeautifulSoup(source.content, 'lxml')
 
